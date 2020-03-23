9299d859576ee0139e7b5ee336876836073ee327
tsharon-byte <sharonova_t@inbox.ru>
2020-03-23 17:43:43 +0300

    Updates

---
+++ precache-manifest.bdd185ac9a17b8387d9334422bea32ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50d09720ab02ad7a57ec72d678df1590",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3afc9a7f5c5b2b1e034",
+    "url": "/static/css/main.ddec9569.chunk.css"
+  },
+  {
+    "revision": "023fda57fd48f804c5f3",
+    "url": "/static/js/2.11681550.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.11681550.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3afc9a7f5c5b2b1e034",
+    "url": "/static/js/main.517885dc.chunk.js"
+  },
+  {
+    "revision": "46e1ce656ac57e3635d1",
+    "url": "/static/js/runtime-main.876764e1.js"
+  }
+]);
